0c979a2b30713c4cb0fdba2aeadc3bc2c83beaed
Bharath <kumar.bharath.k436@gmail.com>
2022-12-31 11:31:31 +0000

    Adding Bhart Prodject

---
+++ Bharat_Project/src/main/java/Bharat_Project/Bharat_Project/BharatProjectApplication.java
@@ -0,0 +1,13 @@
+package Bharat_Project.Bharat_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatProjectApplication.class, args);
+	}
+
+}
---
+++ Bharat_Project/src/test/java/Bharat_Project/Bharat_Project/BharatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Bharat_Project.Bharat_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
